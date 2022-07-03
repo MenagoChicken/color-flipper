76dd9d67d5f96a3b60b19eea42854a4abfb52b3a
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-03 16:34:53 +0100

    Working clean JS
    Checking if JS works. Now it works.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+const colors = [
+  "aquamarine",
+  "rgb(240,230,140)",
+  "#20b2aa",
+  "rgb(255,69,0)",
+  "tomato",
+  "#a0522d",
+];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+console.log(btn);
+console.log(color);
