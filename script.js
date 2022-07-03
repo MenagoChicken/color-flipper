9abb6e0c8dd06e27b748ab69dec8938f46c8012d
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-03 16:53:18 +0100

    Basic - DONE
    Basic version done.

--- script.js
+++ script.js
@@ -12,8 +12,13 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
   //get random numbner between 0-5
-  const randomNumber = 4;
+  let randomNumber = getRandomNumber();
+  console.log(randomNumber);
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
   color.style.color = colors[randomNumber];
 });
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
