cc73e25dea223053d1fdc8a016e6f5d9759bf0f0
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-04 21:20:14 +0100

    random-number&hex-value
    getRandomNumber function and getHexValue function

--- hex.js
+++ hex.js
@@ -5,3 +5,20 @@ const color = document.querySelector(".color");
 
 console.log(btn);
 console.log(color);
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
+
+console.log(getRandomNumber());
+
+function getHexValue() {
+  let hexValue = "#";
+  for (let i = 0; i < 6; i++) {
+    hexValue = hexValue + hex[getRandomNumber()];
+  }
+  return hexValue;
+  console.log(hexValue);
+}
+
+console.log(getHexValue());
