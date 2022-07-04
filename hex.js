26ef207432db5571a47122a3bbe346b8e346d121
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-04 21:27:05 +0100

    Finished-version?
    I think it is finished.

--- hex.js
+++ hex.js
@@ -1,4 +1,4 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
@@ -10,8 +10,6 @@ function getRandomNumber() {
   return Math.floor(Math.random() * hex.length);
 }
 
-console.log(getRandomNumber());
-
 function getHexValue() {
   let hexValue = "#";
   for (let i = 0; i < 6; i++) {
@@ -21,4 +19,10 @@ function getHexValue() {
   console.log(hexValue);
 }
 
-console.log(getHexValue());
+btn.addEventListener("click", function () {
+  let hexValue = getHexValue();
+  console.log(hexValue);
+  document.body.style.backgroundColor = getHexValue();
+  color.textContent = getHexValue();
+  color.style.color = getHexValue();
+});
